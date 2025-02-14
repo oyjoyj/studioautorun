7d6ed4bd991d33071a5d72806198e7fc69cba55d
oyj <2785089588@qq.com>
2025-02-14 18:40:17 +0800

    增加了切换测试环境、切换去紫边、更改缩略图进程名称

--- openclose.py
+++ openclose.py
@@ -5,6 +5,20 @@ import time
 from pyautogui import click
 
 import monitorfile
+import ctypes,sys
+import re
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+    
+def runas_admin():
+    if is_admin():
+        return
+    #提升权限
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
 
 def cleancache():
     os.system(r"rd /s /q C:\Users\insta360\Documents\Insta360\Studio\Project")
@@ -40,15 +54,99 @@ def forceclosestudio():
     os.system("taskkill /f /im \"Insta360 Studio.exe\"")
     time.sleep(5)
 
+def changethumbnailname(choice):
+    path = r"C:\Program Files\Insta360 Studio"
+    source = path + r"\insta360-thumbnail-service.exe"
+    dest = path + r"\111.exe"
+    if choice == 1:
+        os.rename(source, dest)
+        
+    elif choice == 2:
+        os.rename(dest, source)
+        
+def cloud_service_test(inipath):
+    os.chdir(inipath)
+    ifopen = int(input("1:open 2:close\n"))
+    with open("startup.ini", "r") as f:
+        content = f.read()
+        if 'test_environment' in content:
+            if ifopen == 1:
+                if 'test_environment=true' in content:
+                    f.close()
+                    print("test environment is already open")
+                else:
+                    content = content.replace('test_environment=false', 'test_environment=true')
+                    with open("startup.ini", "w") as f:
+                        f.write(content)
+                    print("test environment is open")
+            elif ifopen == 2:
+                if 'test_environment=false' in content:
+                    f.close()
+                    print("test environment is already close")
+                else:
+                    content = content.replace('test_environment=true', 'test_environment=false')
+                    with open("startup.ini", "w") as f:
+                        f.write(content)
+                    print("test environment is close")
+        else:
+            with open("startup.ini", "a") as f:
+                f.write("[cloud_service]\n")
+                if ifopen == 1:
+                    f.write("test_environment=true\n")
+                elif ifopen == 2:
+                    f.write("test_environment=false\n")
+            print("test environment is added")
+
+def defringechange(inipath):
+    os.chdir(inipath)
+    ifopen = int(input("1:open 2:close\n"))
+    with open("startup.ini", "r") as f:
+        content = f.read()
+        if 'defringe' in content:
+            if ifopen == 1:
+                if 'defringe=true' in content:
+                    f.close()
+                    print("defringe is already open")
+                else:
+                    content = content.replace('defringe=false', 'defringe=true')
+                    with open("startup.ini", "w") as f:
+                        f.write(content)
+                    print("defringe is open")
+            elif ifopen == 2:
+                if 'defringe=false' in content:
+                    f.close()
+                    print("defringe is already close")
+                else:
+                    content = content.replace('defringe=true', 'defringe=false')
+                    with open("startup.ini", "w") as f:
+                        f.write(content)
+                    print("defringe is close")
+        else:
+            with open("startup.ini", "a") as f:
+                f.write("[filter]\n")
+                if ifopen == 1:
+                    f.write("defringe=true\n")
+                elif ifopen == 2:
+                    f.write("defringe=false\n")
+            print("defringe is added")
+
 if __name__ == "__main__":
     #获取输入值
-    print("1:open 2:close 3:cleancache 4:forceclose")
-    choice = int(input())
-    if choice == 1:
-        openstudio(0)
-    elif choice == 2:
-        closestudio()
-    elif choice == 3:
-        cleancache()
-    elif choice == 4:
-        forceclosestudio()
+    if is_admin() == False:
+        print("1:open 2:close 3:cleancache 4:forceclose 5:change thumbnail name 6:cloud service test environment change")
+        choice = int(input())
+        if choice == 1:
+            openstudio(0)
+        elif choice == 2:
+            closestudio()
+        elif choice == 3:
+            cleancache()
+        elif choice == 4:
+            forceclosestudio()
+        elif choice == 5:
+            runas_admin()
+        elif choice == 6:
+            cloud_service_test(r"C:\Users\insta360\AppData\Local\Insta360\Insta360 Studio")
+    else:
+        ch = int(input("1:change 2:recover\n"))
+        changethumbnailname(ch)
